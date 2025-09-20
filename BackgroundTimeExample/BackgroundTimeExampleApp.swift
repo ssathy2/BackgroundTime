bd588d23b26d5e0fa797f2a42ab5f5babee10d3b
Siddharth Sathyam <ssathy2@gmail.com>
2025-09-19 23:13:46 -0500

    Init commit

---
+++ BackgroundTimeExample/BackgroundTimeExampleApp.swift
@@ -0,0 +1,32 @@
+//
+//  BackgroundTimeExampleApp.swift
+//  BackgroundTimeExample
+//
+//  Created by Siddharth Sathyam on 9/19/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BackgroundTimeExampleApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BackgroundTimeExampleTests/BackgroundTimeExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  BackgroundTimeExampleTests.swift
+//  BackgroundTimeExampleTests
+//
+//  Created by Siddharth Sathyam on 9/19/25.
+//
+
+import Testing
+
+struct BackgroundTimeExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
